de667fcf923a85969b39485b9b1e6ffc1789d8c5
park <msmn1729@gmail.com>
2020-12-13 22:35:56 +0900

    refactor(step-3): 불필요한 주석, 코드 삭제

--- step-3.cpp
+++ step-3.cpp
@@ -10,7 +10,6 @@ char cube[6][3][3] =
     {{'Y','Y','Y'},{'Y','Y','Y'},{'Y','Y','Y'}},
     {{'R','R','R'},{'R','R','R'},{'R','R','R'}} };
 
-
 void printLine(int i, int k) {
     int j;
     for(j=0; j<3; j++) {
@@ -45,46 +44,10 @@ void printCube() {
     }
 }
 
-void leftShift(char cube[][3], int row) {
-    int i;
-    char tmp = cube[row][0]; //기억해둬야할 문자
-    for(i=0; i<2; i++) {
-        cube[row][i] = cube[row][i+1];
-    }
-    cube[row][2] = tmp;
-}
-
-void rightShift(char cube[][3], int row) {
-    int i;
-    char tmp = cube[row][2];
-    for(i=2; i>=1; i--) {
-        cube[row][i] = cube[row][i-1];
-    }
-    cube[row][0] = tmp;
-}
-
-void upShift(char cube[][3], int col) {
-    int i;
-    char tmp = cube[0][col];
-    for(i=0; i<2; i++) {
-        cube[i][col] = cube[i+1][col];
-    }
-    cube[2][col] = tmp;
-}
-
-void downShift(char cube[][3], int col) {
-    int i;
-    char tmp = cube[2][col];
-    for(i=2; i>=1; i--) {
-        cube[i][col] = cube[i-1][col];
-    }
-    cube[0][col] = tmp;
-}
-
 int main() {
     string word;
     int i;
-    printCube();
+    
     while(true) {
         cout << "CUBE> ";
         string command;
@@ -94,38 +57,40 @@ int main() {
             return 0;
         }
         
-        //        for(i=0; i<command.size(); i++) {
-        //            if(command[i] == 'U' && command[i+1] == '\'') {
-        //                rightShift(cube, 0);
-        //                cout << command[i];
-        //                continue;
-        //            }
-        //            else if(command[i] == 'U') leftShift(cube, 0);
-        //
-        //            else if(command[i] == 'B' && command[i+1] == '\'') {
-        //                rightShift(cube, 2);
-        //                cout << command[i];
-        //                continue;
-        //            }
-        //            else if(command[i] == 'B') leftShift(cube, 2);
-        //
-        //            else if(command[i] == 'R' && command[i+1] == '\'') {
-        //                downShift(cube, 2);
-        //                cout << command[i];
-        //                continue;
-        //            }
-        //            else if(command[i] == 'R') upShift(cube, 2);
-        //
-        //            else if(command[i] == 'L' && command[i+1] == '\'') {
-        //                downShift(cube, 0);
-        //                cout << command[i];
-        //                continue;
-        //            }
-        //            else if(command[i] == 'L') upShift(cube, 0);
-        //
-        //            cout << command[i] << '\n';
-        //            printCube(cube);
-        //        }
+        for(i=0; i<command.size(); i++) {
+            if(command[i] == 'F' && command[i+1] == '\'') {
+                cout << command[i];
+                continue;
+            }
+            else if(command[i] == 'F') {}
+            else if(command[i] == 'R' && command[i+1] == '\'') {
+                cout << command[i];
+                continue;
+            }
+            else if(command[i] == 'R') {}
+            else if(command[i] == 'U' && command[i+1] == '\'') {
+                cout << command[i];
+                continue;
+            }
+            else if(command[i] == 'U') {}
+            else if(command[i] == 'B' && command[i+1] == '\'') {
+                cout << command[i];
+                continue;
+            }
+            else if(command[i] == 'B') {}
+            else if(command[i] == 'L' && command[i+1] == '\'') {
+                cout << command[i];
+                continue;
+            }
+            else if(command[i] == 'L') {}
+            else if(command[i] == 'D' && command[i+1] == '\'') {
+                cout << command[i];
+                continue;
+            }
+            else if(command[i] == 'D') {}
+            cout << command[i] << '\n';
+            printCube();
+        }
     }
     return 0;
 }
